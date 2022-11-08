22613aea08a87de9cf887beca4783a026efca421
emsthf <deunsol916@gmail.com>
2022-11-08 13:22:42 +0900

    FEAT: 값 타입 적용하기 1. 변경이 잘 안되고 공통적인 부분을 값 타입으로 빼서 사용(꼭 공통부분이 아니라도 상관X) 2. equals(), hashCode() 오버라이드 할 때에는 getter를 사용해서 프록시를 사용해도 괜찮도록 만들자

--- src/main/java/jpaBook/jpashop/domain/Member.java
+++ src/main/java/jpaBook/jpashop/domain/Member.java
@@ -12,9 +12,9 @@ public class Member extends BaseEntity {
     @Column(name = "MEMBER_ID")  // 회사마다 대소문자 룰이 다르다.
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded  //값 타입 객체에 @Embadable 어노테이션이 붙어있으면 생략해도 되지만 명확하게 값 타입이라는 것을 알리기 위해 사용하는 것이 좋다.
+    private Address address;
 
     @OneToMany(mappedBy = "member")  // 연관관계의 주인이 되는 필드 값(Order의 member)
     private List<Order> orders = new ArrayList<>();  // JPA 관례 상 배열 초기화. 그래야 데이터 없이 넣어서 NullPointerException 방지 가능
@@ -35,27 +35,19 @@ public class Member extends BaseEntity {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
-    public String getStreet() {
-        return street;
+    public List<Order> getOrders() {
+        return orders;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 }
